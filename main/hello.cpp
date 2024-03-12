a2961d9a6f75c3616e40498b8592ba6de1f9ea15
Nitish0602 <151609905+Nitish0602@users.noreply.github.com>
2024-03-12 16:59:45 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,6 +6,6 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run\n";
-  cout << "Hello, I am PES2UG21CS349\n";
+  cout << "Hello, I am PES2UG21CS349_nitish\n";
   return 0;
 }
